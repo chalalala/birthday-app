6f8f23ad310f3a23eb7a5aee5eb747e1132324fd
chalalala <neko_hime1212@yahoo.com>
2022-01-04 00:03:45 +0700

    feat: create sidebar

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,15 @@ import { React } from 'react';
 
 export default function Header() {
    return (
-      <div class="header flex justify-space-between">
-         <div class="flex align-center header__left">
-            <span class="material-icons">menu</span>
-            <span class="header__app-name">Birthday Calendar</span>
+      <div className="header flex justify-space-between">
+         <div className="flex align-center header__left">
+            <span className="material-icons">menu</span>
+            <span className="header__app-name">Birthday Calendar</span>
          </div>
 
-         <div class="flex header__right">
-            <span class="material-icons">notifications</span>
-            <span class="material-icons">account_circle</span>
+         <div className="flex header__right">
+            <span className="material-icons">notifications</span>
+            <span className="material-icons">account_circle</span>
          </div>         
       </div>
    )
---
+++ src/components/Sidebar.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+const SidebarLink = ({ icon, active }) => {
+   return (
+      <div className={`flex justify-center sidebar__link ${active ? "active" : ""}`}>
+         <span className="material-icons">{ icon }</span>
+      </div>
+   )
+}
+
+export default function Sidebar() {
+   return (
+      <div className="flex flex-column sidebar">
+         <SidebarLink icon="event" active={true} />
+         <SidebarLink icon="list_alt" />
+         <SidebarLink icon="settings" />
+      </div>
+   )
+}
