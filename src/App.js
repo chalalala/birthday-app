58a5dd8562e868626fe589df5d21da134eaecaa5
chalalala <neko_hime1212@yahoo.com>
2022-01-04 18:09:48 +0700

    feat: add register page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import 'material-icons/iconfont/material-icons.css';
 import './styles/main.scss';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthContextProvider } from './utils/firebase';
 import CalendarPage from './pages/calendar';
+import RegisterPage from './pages/register';
 
 function App() {
   return (
-    <div className="App">
-      <CalendarPage />
-    </div>
+    <AuthContextProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<CalendarPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </Router>
+    </AuthContextProvider>
   );
 }
 
---
+++ src/pages/register.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import AuthenticatingLayout from '../components/AuthenticatingLayout';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
+const RegisterForm = () => {
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+      const { email, password } = e.target.elements;
+      const auth = getAuth();
+      try {
+         await createUserWithEmailAndPassword(auth, email.value, password.value);
+      }
+      catch (e) {
+         alert(e.message);
+      }
+   }
+
+   return (
+      <form onSubmit={handleSubmit}>
+         <div class="form__field">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" placeholder="name@domain.com" />
+         </div>
+
+         <div class="form__field">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" />
+         </div>
+
+         <button type="submit">Submit</button>
+      </form>
+   )
+}
+
+export default function RegisterPage() {
+   
+   return (
+      <AuthenticatingLayout title="Sign Up">
+         <p>Already have an account? <a class="form__link">Sign in</a></p>
+         <RegisterForm />
+      </AuthenticatingLayout>
+   )
+}
