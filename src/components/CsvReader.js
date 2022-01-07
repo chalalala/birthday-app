56e1576afaed4a6dc9e91899da1f4d9cf727b7b5
chalalala <neko_hime1212@yahoo.com>
2022-01-07 20:27:46 +0700

    feat: add function to create collection on firebase

--- src/components/CsvReader.js
+++ src/components/CsvReader.js
@@ -1,8 +1,16 @@
 import { useState, React } from 'react';
+import { firebaseApp, useAuthState } from '../utils/firebase';
+import { getFirestore, collection, setDoc, doc } from 'firebase/firestore';
+import { useSnackbar } from 'notistack';
 
 export default function CsvReader() {
+   const { enqueueSnackbar } = useSnackbar();
+
    const [CSVFile, setCSVFile] = useState();
-   const [CSVArray, setCSVArray] = useState([])
+   const [CSVArray, setCSVArray] = useState([]);
+
+   const { user } = useAuthState();
+   const db = getFirestore(firebaseApp);
 
    const onFileUpload = (e) => {
       setCSVFile(e.target.files[0]);
@@ -17,10 +25,16 @@ export default function CsvReader() {
          reader.onload = (e) => {
             const text = e.target.result;
             processCSV(text);
+            CSVArray.forEach(entry => saveData(entry));
+            enqueueSnackbar('Imported list successfully.', { variant: 'success' });
          }
    
          reader.readAsText(CSVFile);
       }
+   }
+
+   const saveData = async (data) => {      
+      await setDoc(doc(db, user.email, "birthday-list"), data);
    }
 
    const processCSV = (str, delim=',') => {
@@ -40,7 +54,6 @@ export default function CsvReader() {
       }).filter(item => item !== undefined);
 
       setCSVArray(newArray);
-      console.log(CSVArray);
    }
 
    return (
