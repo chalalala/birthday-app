486b43db9b5f08c4ad5e52041b6fd9a7908274e5
chalalala <neko_hime1212@yahoo.com>
2022-02-18 21:08:57 +0700

    refactor: change component name

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Layout from '../components/Layout';
 import CsvReader from '../components/CsvReader';
 import BirthdayCalendar from "../components/BirthdayCalendar";
+import ProtectedPage from '../components/ProtectedRoute';
 
 export default function CalendarPage() {
    return (
-      <Layout currentSite="calendar">
-         <CsvReader />
-         <BirthdayCalendar />
-      </Layout>
+      <ProtectedPage>
+         <Layout currentSite="calendar">
+            <CsvReader />
+            <BirthdayCalendar />
+         </Layout>
+      </ProtectedPage>
    )
 }
