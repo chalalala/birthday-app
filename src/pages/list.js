8ba8f895093560557763865dc493aba6c4dbb44e
chalalala <neko_hime1212@yahoo.com>
2022-02-19 22:49:46 +0700

    feat: display birthday list

--- src/pages/list.js
+++ src/pages/list.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import BirthdayList from '../components/BirthdayList'
 import ProtectedPage from '../components/ProtectedRoute'
+import Layout from '../components/Layout'
+import CsvReader from '../components/CsvReader'
 
 export default function ListPage() {
   return (
       <ProtectedPage>
-        <BirthdayList />
+        <Layout currentSite="list">
+          <CsvReader />
+          <BirthdayList />
+        </Layout>
       </ProtectedPage>
   )
 }
