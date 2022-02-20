2020cae49110bb2de8254d203953a413282958ba
chalalala <neko_hime1212@yahoo.com>
2022-02-20 21:16:58 +0700

    feat: update birthday list table

--- src/components/BirthdayList.tsx
+++ src/components/BirthdayList.tsx
@@ -1,3 +1,10 @@
+import { Paper, TableFooter, TablePagination } from '@mui/material';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 import { doc, getDoc } from 'firebase/firestore';
 import moment from 'moment';
 import React, { useEffect, useState } from 'react';
@@ -7,7 +14,19 @@ import { db } from '../utils/firebase';
 
 export default function BirthdayList() {
   const { user } = useAuthState();
-  const [list, setList] = useState([]);
+  const [birthdayList, setBirthdayList] = useState([]);
+
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [page, setPage] = React.useState(0);
+
+  const handleChangePage = (event: any, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: any) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   const getEventList = async () => {
 		const docRef = doc(db, user.email, "birthday-list");
@@ -15,8 +34,7 @@ export default function BirthdayList() {
 
 		if (birthdayDoc.exists()) {
       let data = birthdayDoc.data();
-      setList(data.birthdayList);
-			console.log(data);
+      setBirthdayList(data.birthdayList);
 		} else {
 			console.log("No data");
 		}
@@ -27,18 +45,43 @@ export default function BirthdayList() {
   }, []);
 
   return (
-    <table>
-      <tr>
-        <th>Name</th>
-        <th>Birthday</th>
-      </tr>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+            <TableRow>
+              <TableCell>No</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>DOB</TableCell>
+              <TableCell size="small">Actions</TableCell>
+            </TableRow>
+        </TableHead>
 
-      { list.length > 0 && list.map((entry: IEntry) => (
-        <tr key={entry.name + String(Date.now())}>
-          <td>{entry.name}</td>
-          <td>{moment(new Date(entry.dob)).format('DD/MM/YYYY')}</td>
-        </tr>
-      ))}
-    </table>
+        <TableBody>
+          { birthdayList.length > 0 && 
+            birthdayList.slice(rowsPerPage*page, rowsPerPage*page + rowsPerPage).map((entry: IEntry, index: Number) => (
+            <TableRow key={entry.name + String(Date.now())}>
+              <TableCell>{index}</TableCell>
+              <TableCell>{entry.name}</TableCell>
+              <TableCell>{moment(new Date(entry.dob)).format('DD/MM/YYYY')}</TableCell>
+              <TableCell size="small"></TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+              colSpan={3}
+              count={birthdayList.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </TableContainer>
   )
 }
