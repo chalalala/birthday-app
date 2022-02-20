adb126b84d0bc5cadba2442361fceb08e93c685c
chalalala <neko_hime1212@yahoo.com>
2022-02-20 12:30:10 +0700

    refactor: change from js to tsx files

---
+++ src/components/SidebarLink.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+interface Props {
+   icon: string,
+   name: string,
+   to?: string,
+   currentSite: string
+}
+
+export const SidebarLink = (props:Props) => {
+   const { icon, name, currentSite, to = '/' } = props;
+
+   return (
+      <Link to={to} className={`flex justify-center sidebar__link ${name === currentSite ? "active" : ""}`}>
+            <span className="material-icons">{ icon }</span>
+      </Link>
+   )
+}
