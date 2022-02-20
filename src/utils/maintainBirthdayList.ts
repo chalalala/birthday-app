468458ce712816fc11eb14e3718fe6093af7ed88
chalalala <neko_hime1212@yahoo.com>
2022-02-20 17:53:01 +0700

    feat: add function to remove event

--- src/utils/maintainBirthdayList.ts
+++ src/utils/maintainBirthdayList.ts
@@ -5,7 +5,7 @@ import { db } from "./firebase";
 export const uploadBirthdayList = async (birthdayList: Array<IEntry>, user: any) => {
    await setDoc(doc(db, user.email, "birthday-list"), { birthdayList })
    .then(() => {
-      console.log("Imported ", birthdayList);
+      console.log("Uploaded ", birthdayList);
    })
    .catch((e) => {
       console.log(e);
