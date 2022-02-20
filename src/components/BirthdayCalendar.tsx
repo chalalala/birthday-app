290ef8bc6452aec459225a4ff5b582faa13eeca2
chalalala <neko_hime1212@yahoo.com>
2022-02-20 15:26:18 +0700

    refactor: remove unused script

--- src/components/BirthdayCalendar.tsx
+++ src/components/BirthdayCalendar.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from "react";
 import { Day, Inject, Month, ScheduleComponent, ViewDirective, ViewsDirective, Week } from '@syncfusion/ej2-react-schedule';
 import { doc, getDoc } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 import { useAuthState } from "../contexts/AuthContext";
-import { db } from '../utils/firebase';
-import recurDate from '../utils/recurDate';
-import { IEvent } from "../types/IEvent";
 import { IEntry } from "../types/IEntry";
+import { IEvent } from "../types/IEvent";
+import { db } from '../utils/firebase';
 
 export default function BirthdayCalendar() {
     const { user } = useAuthState();
@@ -17,7 +16,8 @@ export default function BirthdayCalendar() {
             Subject: entry.nickname !== '' ? entry.nickname : entry.name,
             StartTime: date,
             EndTime: new Date(date),
-            IsAllDay: true
+            IsAllDay: true,
+            RecurrenceRule: 'FREQ=YEARLY;INTERVAL=1'
         }
     )
 
@@ -27,18 +27,24 @@ export default function BirthdayCalendar() {
 
         if (birthdayDoc.exists()) {
             let birthdayList = [...birthdayDoc.data().birthdayList];
-            let newArray = new Array<IEvent>();
-            
-            birthdayList.forEach(entry => {
-                let bday = new Date(entry.dob);
-                let recurDates = recurDate(bday);
-                let events = recurDates.map((date, index) => createEventObject(index, date, entry));
-                newArray = newArray.concat(events);
-            })
+            let newArray = birthdayList.map((entry, index) => createEventObject(index, new Date(entry.dob), entry));
             setEventList(newArray);
         }
         else {
             console.log("No data");
+        }
+    }
+
+    const removeEvent = (index: Number) => {
+        let newEventList = eventList.filter(event => event.Id !== index);
+        setEventList(newEventList);
+    }
+
+    const onActionComplete = (action: string) => {
+        switch (action) {
+            case "eventRemove": {
+                // removeEvent();
+            }
         }
     }
 
@@ -56,6 +62,7 @@ export default function BirthdayCalendar() {
                     allowDeleting: true,
                     allowEditing: true
                 }}
+                actionComplete={onActionComplete}
                 cssClass='birthday-calendar'
             >
                 <ViewsDirective>
