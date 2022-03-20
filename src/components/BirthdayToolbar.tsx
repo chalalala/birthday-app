45b211b07eed3b2cbfd17f8e7c39bb9006b0707b
chalalala <neko_hime1212@yahoo.com>
2022-03-21 00:11:37 +0700

    refactor: create common component

--- src/components/BirthdayToolbar.tsx
+++ src/components/BirthdayToolbar.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
+import React from "react";
 
 type Props = {
-   title?: string;
-   setOpenModal: (state: boolean) => void;
-   exportData: () => void;
-}
+	title?: string;
+	handleOpen: () => void;
+	exportData: () => void;
+};
 
 export const BirthdayToolbar = (props: Props) => {
-   const { title, setOpenModal, exportData } = props;
-   const justifyClass = title ? "justify-space-between" : "justify-flex-end";
+	const { title, handleOpen, exportData } = props;
+	const justifyClass = title ? "justify-space-between" : "justify-flex-end";
 
-   return (
-      <div className={`flex ${justifyClass} birthday-toolbar`}>
-         { title && <h1 className="birthday-toolbar__title">{ title }</h1> }
-         <div className="flex birthday-toolbar__actions">
-            <button onClick={() => setOpenModal(true)} className="text-button">Import</button>
-            <button onClick={exportData} className="text-button">Export</button>
-            <button className="primary-button birthday-toolbar__button">Add Entry</button>
-         </div>
-      </div>
-   )
-}
+	return (
+		<div className={`flex ${justifyClass} birthday-toolbar`}>
+			{title && <h1 className="birthday-toolbar__title">{title}</h1>}
+			<div className="flex birthday-toolbar__actions">
+				<button onClick={handleOpen} className="text-button">
+					Import
+				</button>
+				<button onClick={exportData} className="text-button">
+					Export
+				</button>
+				<button className="primary-button birthday-toolbar__button">Add Entry</button>
+			</div>
+		</div>
+	);
+};
