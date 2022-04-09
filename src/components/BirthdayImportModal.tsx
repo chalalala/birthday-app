f25f4321916734505fc2c87bada0bd5c78a23315
chalalala <neko_hime1212@yahoo.com>
2022-04-09 20:15:33 +0700

    fix: update import form

--- src/components/BirthdayImportModal.tsx
+++ src/components/BirthdayImportModal.tsx
@@ -1,5 +1,5 @@
-import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
 import React from "react";
+import { DialogContentText, FormControl } from "@mui/material";
 import FormModal from "./commons/FormModal";
 
 interface Props {
@@ -15,14 +15,14 @@ export const BirthdayImportModal = (props: Props) => {
 	return (
 		<FormModal title="Import list by XLSX" open={open} handleClose={handleClose} handleSubmit={onFileSubmit}>
 			<DialogContentText>
-				<p>
-					The file must include two columns <b>name</b> and <b>dob</b>, written as plain text.
-					<br />
-					The date must be in format <b>MM/DD/YYYY</b>.
-				</p>
-				<input type="file" accept=".xlsx" onChange={onFileUpload} />
+				The file must include two columns <b>name</b> and <b>dob</b>, written as plain text.
 				<br />
+				The date must be in format <b>MM/DD/YYYY</b>.
 			</DialogContentText>
+
+			<FormControl fullWidth sx={{ mt: 2 }}>
+				<input type="file" accept=".xlsx" onChange={(e) => onFileUpload(e)} />
+			</FormControl>
 		</FormModal>
 	);
 };
