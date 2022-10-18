6b65d4e33be79ac57349e76ccd5c6594d784f58a
thuanndqsoft <thuannd@qsoftvietnam.com>
2022-10-18 22:49:08 +0700

    feat: create upcoming birthday section

---
+++ src/components/UpcomingSection.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import { useAuthState } from '../contexts/AuthContext';
+import '../styles/components/_upcomingSection.scss';
+import { IEntry } from '../types/IEntry';
+import { getUpcomingBirthdayList } from '../utils/maintainBirthdayList';
+import { BirthdayCard } from './BirthdayCard';
+
+export const UpcomingSection = () => {
+  const { user } = useAuthState();
+  const [birthdayList, setBirthdayList] = useState<IEntry[]>([]);
+
+  const fetchIncomingBirthdays = async () => {
+    const list: IEntry[] = await getUpcomingBirthdayList(user);
+    setBirthdayList(list);
+  };
+
+  useEffect(() => {
+    fetchIncomingBirthdays();
+  }, [user.email]);
+
+  return (
+    <div>
+      <div className="header-title">Upcoming Birthdays</div>
+      {birthdayList.map((birthday: IEntry) => (
+        <BirthdayCard birthday={birthday} />
+      ))}
+    </div>
+  );
+};
--- src/utils/maintainBirthdayList.ts
+++ src/utils/maintainBirthdayList.ts
@@ -1,4 +1,5 @@
 import { doc, getDoc, setDoc } from 'firebase/firestore';
+import moment from 'moment';
 import { IEntry } from '../types/IEntry';
 import { db } from './firebase';
 
@@ -23,5 +24,22 @@ export const getBirthdayList = async (user: any, setFunc: Function) => {
     setFunc(birthdayDoc.data().birthdayList);
   } else {
     console.log('No data');
+  }
+};
+
+const RANGE_OF_UPCOMING_BIRTHDAYS = 7;
+
+export const getUpcomingBirthdayList = async (user: any) => {
+  const docRef = doc(db, user.email, 'birthday-list');
+  const birthdayDoc = await getDoc(docRef);
+
+  if (birthdayDoc.exists()) {
+    const startDate = moment();
+    const endDate = moment().add(RANGE_OF_UPCOMING_BIRTHDAYS, 'day');
+    return [...birthdayDoc.data().birthdayList].filter((item) =>
+      moment(item.dob, 'MM/DD/YYYY').isBetween(startDate, endDate),
+    ) as IEntry[];
+  } else {
+    return [];
   }
 };
