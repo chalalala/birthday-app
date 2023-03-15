50f5b069ab4dc12585ab4c6ba338108eec9e2d56
Đoàn Tuyết Mai <neko_hime1212@yahoo.com>
2023-03-15 23:54:29 +0700

    fix: update diff date function to ignore year (#18)

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -3,13 +3,16 @@ import moment from 'moment';
 export const DATE_FORMAT = 'MM/DD/YYYY';
 
 /**
- *  Get difference in dates excluded time
+ *  Get difference in dates ignore year and time
  */
 export const getDiffDate = (
   currentDate: moment.MomentInput,
   comparedDate: moment.MomentInput,
 ) => {
+  const momentComparedDate = moment(comparedDate);
+
   return moment(currentDate, DATE_FORMAT)
     .startOf('day')
-    .diff(moment(comparedDate).startOf('day'), 'days');
+    .year(momentComparedDate.year())
+    .diff(momentComparedDate.startOf('day'), 'days');
 };
